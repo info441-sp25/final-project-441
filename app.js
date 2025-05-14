ad1625a38b0cf27a497d28488fc841c862a5d0c1
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-05-14 15:05:02 -0700

    models middleware

--- app.js
+++ app.js
@@ -20,6 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) =>{
+    req.models = models
+    next()
+})
+
 app.use('/users', usersRouter);
 
 export default app;
