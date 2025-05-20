89731e52bac37546014073597b810b89b16d4981
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-05-20 13:43:33 -0700

    implementing get for saved courses and made js folder

--- routes/api/v1/controllers/users.js
+++ routes/api/v1/controllers/users.js
@@ -1,9 +1,19 @@
 import express from 'express';
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// GET saved courses
+// @pre: GET api/v1/users/saved?userID=...
+// @post: returns JSON arr of saved course info
+router.get('/saved', async (req, res) => {
+  const user = await req.models.User.findOne({userId : req.query.userId})
+
+  if (!user) {
+    return res.status(404).json({error: "User not found"})
+  }
+  // arr of saved courses
+  const savedPromises = await Promise.all(user.savedCourses.map(async course => {
+    return await req.models.Course.findOne({courseId : course})
+  }))
+})
 
 export default router;
