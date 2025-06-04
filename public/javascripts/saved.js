6fec9dab644ddb2abf28797ece832770129b2ba2
sanjjjp <sanjjjp@uw.edu>
2025-06-04 11:33:10 -0700

    formatting css

--- public/javascripts/saved.js
+++ public/javascripts/saved.js
@@ -5,7 +5,7 @@ async function initSaved() {
     const identityRes = await fetch("api/v1/user/myIdentity");
     const identity = await identityRes.json();
 
-    console.log("this is the identity", identity)
+    console.log("this is the identity", identity);
 
     if (!identity?.userInfo?.username) {
       document.getElementById("saved-results").innerText = "Please log in to view saved courses.";
@@ -15,24 +15,22 @@ async function initSaved() {
     const userId = identity.userInfo.username;
     const savedRes = await fetch(`/api/v1/user/saved?userId=${userId}`);
     const json = await savedRes.json();
-    const savedCourses = json.saved
+    const savedCourses = json.saved;
 
     const container = document.getElementById("saved-results");
     if (!savedCourses || savedCourses.length === 0) {
       container.innerHTML = "<p>No saved courses yet.</p>";
     } else {
-      container.innerHTML = savedCourses.map(course => 
-      `
-      <div class="card" id="${course.courseId}" style="margin-bottom: 1rem;">
+      container.innerHTML = savedCourses.map(course => `
+        <div class="card" id="${course.courseId}">
           <div style="display: flex; justify-content: space-between;">
-              <h3>${course.courseId}: ${course.courseTitle}</h3>
+            <h3>${course.courseId}: ${course.courseTitle}</h3>
           </div>
           <p>College: ${course.courseCollege}</p>
           <p>Credits: ${course.credits}</p>
-          <button onclick="selectCourse('${course.courseId}')"> View Course </button>
-      </div>
-      `
-      ).join("")
+          <button onclick="selectCourse('${course.courseId}')">View Course</button>
+        </div>
+      `).join("");
     }
   } catch (err) {
     console.error("Failed to load saved courses:", err);
