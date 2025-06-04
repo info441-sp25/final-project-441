5aba872468eef363935dfd7fe71f5746fcbe38df
laurakhot <laurak_11@uw.edu      git config --global user.email laurak_11@uw.edu>
2025-06-03 22:21:10 -0700

    added logs

--- routes/api/v1/controllers/users.js
+++ routes/api/v1/controllers/users.js
@@ -83,17 +83,23 @@ router.get('/saved', async (req, res) => {
 // @post: saves courseId to savedCourse arr for user
 router.post('/saved', async (req, res) => {
   try {
+
+    console.log("this is the body", req.body)
     let user = await req.models.User.findOne({userId: req.body.userId})
+    console.log("these are the saved courses", user.savedCourses)
+
     // if course not saved add it
     if (!user.savedCourses.includes(req.body.courseId)) {
       user.savedCourses.push(req.body.courseId)
       await user.save()
+      console.log("course added to user")
       return res.json({status: "success", message: "course added", saved: user.savedCourses})
     } else {
       // if course already added, remove it
       let filteredCourses = user.savedCourses.filter(course => course != req.body.courseId)
       user.savedCourses = filteredCourses
       await user.save()
+      console.log("course deleted from user")
       return res.json({status: "success", message: "course removed", saved: user.savedCourses})
     }
   } catch (err) {
